993feb8c0217226cbe143a3847f2b85b96b42ca0
vovabuddy <golovchansky.vlad@gmail.com>
2025-03-22 14:25:29 +0300

    add features generator dll

---
+++ Beton/Core/DependencyInjections/Attributes/InjectAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Beton.Core.DependencyInjections.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class InjectAttribute : Attribute
+    {
+        
+    }
+}
